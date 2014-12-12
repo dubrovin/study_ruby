954a699b4a3e69de8625ec535b40813480617443
Dubrovin Ruslan <dubrovin.ruslan@gmail.com>
2014-12-12 18:49:17 +0300

    Homework, add comment through ajax.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,24 +6,28 @@ class CommentsController < ApplicationController
 
   def create
     puts params
-    comment = post.comments.new(comment_params)
-    comment.user = current_user
+    @comment = post.comments.new(comment_params)
+    @comment.user = current_user
 
-    if comment.save
+    if @comment.save
       flash[:notice] = "Comment was successflly created"
     else
       flash[:notice] = "Comment wasn't created"
     end
-      
-    redirect_to post_path(post)
-  end	
+
+    respond_to do |format|
+      format.html { redirect_to post_path(post)}
+      format.js
+    end
+
+  end
 
 
   private
   def post
     @post ||= Post.find(params[:post_id])
   end
-  
+
   def comment_params
     params.require(:comment).permit(:title, :message)
   end
